17dce1aed3872e8437ecbdc04ea8635918600265
Ronaldo Rangon <ronaldo.rangon@gmail.com>
2025-07-17 19:23:14 -0300

    Inclusão até a aula 23

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-//PascalCase
-//App
-//HeaderHeading
-//ExemploDeComponente
-import { Fragment } from 'react/jsx-runtime';
-import { Heading } from './components/Heading';
-
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-	console.log('Inicio');
 	return (
-		<Fragment>
-			<Heading attr={124} attr2='string'>
-				Olá Mundo 1
-			</Heading>
-			<Heading>Olá Mundo 2</Heading>
-			<Heading>Olá Mundo 3</Heading>
-			<p>
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-				incidunt, pariatur, quidem id quasi assumenda iste veritatis, in saepe
-				aliquam labore voluptatum mollitia! Dignissimos aspernatur provident
-				facilis suscipit praesentium nostrum.
-			</p>
-		</Fragment>
+		<>
+			<Container>
+				<Logo />
+			</Container>
+
+			<Container>
+				<Menu />
+			</Container>
+		</>
 	);
 }
