eac9422ab21e5f56f02240d748b3418ad7d9ce25
Ronaldo Rangon <ronaldo.rangon@gmail.com>
2025-08-21 15:13:00 -0300

    Inclusao aula 46

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Home } from './pages/Home';
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel'; //comentar com o grupo de estudo sobre o uso do import "type"
+import { TaskContext } from './contexts/TaskContext';
+
+const initialState: TaskStateModel = {
+	tasks: [],
+	secondsRemaining: 0,
+	formattedSecondsRemaining: '00:00',
+	activeTask: null,
+	currentCycle: 0,
+	config: {
+		workTime: 25,
+		shortBreakTime: 5,
+		longBreakTime: 15,
+	},
+};
 
 export function App() {
-	return <Home />;
+	const [state, setState] = useState(initialState);
+
+	return (
+		<TaskContext.Provider value={{ outraCoisa: 321 }}>
+			<Home />
+		</TaskContext.Provider>
+	);
 }
--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,5 +1,9 @@
+import { TaskContext } from '../../contexts/TaskContext';
 import styles from './styles.module.css';
+import { useContext } from 'react';
 
 export function CountDown() {
+	const taskContext = useContext(TaskContext);
+	console.log(taskContext);
 	return <div className={styles.container}>00:00</div>;
 }
