d722e20e6581dce09cc45f8118862735812cecb2
Ronaldo Rangon <ronaldo.rangon@gmail.com>
2025-08-21 09:16:48 -0300

    Inclusao aula 42

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
-import { MainTemplate } from '../../templates/MainTemplate';
+import { MainTemplate } from '../../../src/templates/MainTemplate';
 
 export function Home() {
 	return (
